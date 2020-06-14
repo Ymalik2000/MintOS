2fa8f12bd6968ada99b3f73bca8aa7c792e43d3d
ymalik2000 <ymalik2000@gmail.com>
2020-06-14 17:50:47 -0400

    Working Setup

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,27 @@
-void kernelMain(void *multiboot_structure, unsigned int magicnumber){
-    printf("Hello World");
+void printf(char* str)
+{
+    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+
+
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
+{
+    printf("Welcome to MintOS");
 
     while(1);
 }
-
